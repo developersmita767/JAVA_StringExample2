c556171001bc2171284554f3fab5cf35736e1e52
developersmita767 <156692415+developersmita767@users.noreply.github.com>
2024-07-07 18:31:19 +0530

    Add files via upload

---
+++ StringExample2.java
@@ -0,0 +1,12 @@
+//Write a program to display the length of a given string.
+
+
+public class StringExample2 {
+    public static void main(String[] args)
+    {
+        String str1 = "Micky";
+        int str2 = str1.length( );
+        System.out.println("Length of the string " + str2);
+    }
+    
+}
